83b9988e030f5f20cc71377201c51beb5335991f
tarikul12 <tarikulnishat21@gmail.com>
2024-01-07 23:45:44 +0600

    Single Product

--- store/urls.py
+++ store/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("home/", views.home, name="home"),
     path("store/", views.store, name="store"),
-    path("<slug:category_slug>/", views.store, name="prdducts_by_category"),
+    path("<slug:category_slug>/", views.store, name="products_by_category"),
+    path("<slug:category_slug>/<slug:product_slug>", views.product_detail, name="product_detail"),
 ]
 
--- store/views.py
+++ store/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from store.models import Product
-from category .models import Category
+from category.models import Category
 
 
 def home(request):
@@ -13,17 +13,19 @@ def home(request):
 def store(request, category_slug=None):
     categories = None
     products = None
-    if category_slug != None:
-        categories = get_object_or_404(category, slug=category_slug)
-        products=Product.objects.filter(category=categories,is_available=True)
-        product_count=products.count()
-        
+    if category_slug is not None:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=categories, is_available=True)
+        product_count = products.count()
     else:
-       products = Product.objects.all().filter(is_available=True)
-       product_count = products.count()
+        products = Product.objects.all().filter(is_available=True)
+        product_count = products.count()
 
     context = {
         "products": products,
         "product_count": product_count,
     }
     return render(request, "store/store.html", context)
+
+def product_detail(request,category_slug,product_slug):
+    return render(request,"store/product_detail.html")
