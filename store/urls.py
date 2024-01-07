4c824e95aabbcf824a1a12bf5cf04548ef31282a
tarikul12 <tarikulnishat21@gmail.com>
2024-01-08 01:28:54 +0600

    last change

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path("home/", views.home, name="home"),
     path("store/", views.store, name="store"),
     path("<slug:category_slug>/", views.store, name="products_by_category"),
-    path("<slug:category_slug>/<slug:product_slug>", views.product_detail, name="product_detail"),
+    path('<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
 ]
 
--- store/views.py
+++ store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from store.models import Product
 from category.models import Category
+from django.http import Http404
+
 
 
 def home(request):
@@ -27,5 +29,18 @@ def store(request, category_slug=None):
     }
     return render(request, "store/store.html", context)
 
-def product_detail(request,category_slug,product_slug):
-    return render(request,"store/product_detail.html")
+
+from django.http import Http404
+
+def product_detail(request, category_slug, product_slug):
+    try:
+        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+    except Exception as e:
+        raise e
+
+    context = {
+        "single_product": single_product,
+    }
+
+    return render(request, "store/product_detail.html", context)
+
